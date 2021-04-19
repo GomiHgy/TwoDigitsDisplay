541d865e8b84b0072b3a18e9ac2923f83d406114
GomiHgy <h.furukawa1@gmail.com>
2021-04-19 12:48:17 +0900

    16進数表示機能を実装

--- test.ts
+++ test.ts
@@ -3,12 +3,18 @@ two_digits_display.showTwoDigitsNumber(0)
 let temp = 0.1
 serial.writeNumber(temp)
 serial.writeLine("")
-two_digits_display.showTwoDigitsNumber(0)
+two_digits_display.showTwoDigitsNumber(1.1)
 while (!(input.buttonIsPressed(Button.A))) {
 	
 }
 basic.forever(function () {
+    for (let index = -10; index <= 260; index++) {
+        two_digits_display.showTwoDigitsHexNumber(index)
+        basic.pause(200)
+    }
+
     temp = 0.1
+
     two_digits_display.showTwoDigitsNumber(temp)
     for (let index = 0; index <= 999; index++) {
         two_digits_display.showTwoDigitsNumber(temp)
--- two_digits_display.ts
+++ two_digits_display.ts
@@ -4,11 +4,58 @@
 //% blockId="two_digits_display" block="TwoDigitsDisplay"
 //% weight=100 color=#5C2D91 icon="\uf26c"
 namespace two_digits_display {
+    /**
+     * Display the number on the screen in two digits.
+     * @param value 0.0-99 numbers, eg: 0
+     */
+    //% blockId="show_two_digits_number" block="show number %value"
+    //% parts="two_digits_display"
+    export function showTwoDigitsNumber (value: number) {
+        let _value = Math.round(value * 10)
+        if (_value % 10 == 0) { // 整数
+            if(_value < 100){
+                showDot(true)
+                showTwoColumnNumber(0, Math.trunc(_value / 10) % 10)
+                showTwoColumnNumber(3, 0)
+            }else{
+                showDot(false)
+                showTwoColumnNumber(0, Math.trunc(_value / 100) % 10)
+                showTwoColumnNumber(3, Math.trunc(_value / 10) % 10)
+            }
+        } else {
+            if(_value < 100){
+                showDot(true)
+                showTwoColumnNumber(0, Math.trunc(_value / 10) % 10)
+                showTwoColumnNumber(3, _value % 10)
+            }else{
+                showDot(false)
+                showTwoColumnNumber(0, Math.trunc(_value / 100) % 10)
+                showTwoColumnNumber(3, Math.trunc(_value / 10) % 10)
+            }
+        }
+    }
+
+    /**
+     * Display the hex number on the screen in two digits.
+     * @param value 0-255 numbers, eg: 0
+     */
+    //% blockId="show_two_digits_hex_number" block="show hex number %value"
+    //% parts="two_digits_display"
+    export function showTwoDigitsHexNumber (value: number) {
+        let _value = Math.round(value)
+        if(_value < 0 || _value > 255){
+            return
+        }
+        showDot(false)
+        showTwoColumnNumber(0, Math.trunc(_value >> 4) % 16)
+        showTwoColumnNumber(3, _value % 16)
+    }
+
     function showTwoColumnNumber (startColomn: number, value: number) {
         if (startColomn < 0 || startColomn > 3) {
             return
         }
-        if (value < 0 || value > 9) {
+        if (value < 0 || value > 15) {
             return
         }
 
@@ -133,37 +180,78 @@ namespace two_digits_display {
                 led.plot(startColomn + 1, 3)
                 led.plot(startColomn + 1, 4)
             break;
-        }
-    }
-
-    /**
-     * Display the number on the screen in two digits.
-     * @param value 0.0-99 numbers, eg: 0
-     */
-    //% blockId="show_two_digits_number" block="Display the number on the screen in two digits %value"
-    //% parts="two_digits_display"
-    export function showTwoDigitsNumber (value: number) {
-        let _value = Math.round(value * 10)
-        if (_value % 10 == 0) { // 整数
-            if(_value < 100){
-                showDot(true)
-                showTwoColumnNumber(0, Math.trunc(_value / 10) % 10)
-                showTwoColumnNumber(3, 0)
-            }else{
-                showDot(false)
-                showTwoColumnNumber(0, Math.trunc(_value / 100) % 10)
-                showTwoColumnNumber(3, Math.trunc(_value / 10) % 10)
-            }
-        } else {
-            if(_value < 100){
-                showDot(true)
-                showTwoColumnNumber(0, Math.trunc(_value / 10) % 10)
-                showTwoColumnNumber(3, _value % 10)
-            }else{
-                showDot(false)
-                showTwoColumnNumber(0, Math.trunc(_value / 100) % 10)
-                showTwoColumnNumber(3, Math.trunc(_value / 10) % 10)
-            }
+            case 10:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.unplot(startColomn + 1, 0)
+                led.plot(startColomn + 1, 1)
+                led.plot(startColomn + 1, 2)
+                led.plot(startColomn + 1, 3)
+                led.plot(startColomn + 1, 4)
+            break;
+            case 11:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.plot(startColomn + 1, 0)
+                led.plot(startColomn + 1, 1)
+                led.unplot(startColomn + 1, 2)
+                led.plot(startColomn + 1, 3)
+                led.plot(startColomn + 1, 4)
+            break;
+            case 12:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.plot(startColomn + 1, 0)
+                led.unplot(startColomn + 1, 1)
+                led.unplot(startColomn + 1, 2)
+                led.unplot(startColomn + 1, 3)
+                led.plot(startColomn + 1, 4)
+            break;
+            case 13:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.unplot(startColomn + 1, 0)
+                led.plot(startColomn + 1, 1)
+                led.plot(startColomn + 1, 2)
+                led.plot(startColomn + 1, 3)
+                led.unplot(startColomn + 1, 4)
+            break;
+            case 14:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.plot(startColomn + 1, 0)
+                led.unplot(startColomn + 1, 1)
+                led.plot(startColomn + 1, 2)
+                led.unplot(startColomn + 1, 3)
+                led.plot(startColomn + 1, 4)
+            break;
+            case 15:
+                led.plot(startColomn, 0)
+                led.plot(startColomn, 1)
+                led.plot(startColomn, 2)
+                led.plot(startColomn, 3)
+                led.plot(startColomn, 4)
+                led.plot(startColomn + 1, 0)
+                led.unplot(startColomn + 1, 1)
+                led.plot(startColomn + 1, 2)
+                led.unplot(startColomn + 1, 3)
+                led.unplot(startColomn + 1, 4)
+            break;
         }
     }
 
